ff550853fe7eb13e1c9a2d38987487d1393ef19b
Manisha <manishapanchal5591@gmail.com>
2024-03-02 16:29:13 +0530

    commit after doing modification

--- backend/server.py
+++ backend/server.py
@@ -14,11 +14,7 @@ header = {
   "alg": "HS256",  
   "typ": "JWT"  
 }  
-  
-  
-  
 secret = "Ravipass"  
-  
 @app.route('/login', methods=['POST'])
 @cross_origin(origins=[u"*"])
 def login():
@@ -37,7 +33,7 @@ def login():
 }
         token= jwt.encode(payload, secret, algorithm='HS256', headers=header) 
         # print(data)
-        return jsonify({'data':data,"token":token}),200
+        return jsonify({"token":token}),200
     else:
         return jsonify({'message':'Not exist'}),201
     
@@ -49,7 +45,6 @@ def signup():
     email = data.get('email')
     password = data.get('password')
     username=data.get('name')
-    # print(email,password,username)
     if not email or not password or not username:
         return jsonify({'error': 'Please provide complete information'}), 400
     cursor.execute("select * from users where email=%s",(email,))
@@ -64,9 +59,6 @@ def signup():
 @app.route('/viewall', methods=['POST'])
 @cross_origin(origins=[u"*"])
 def viewall():
-    data = request.get_json()
-    # print(data)
-    email = data.get('email')
     cursor.execute("select * from recipe")
     cols = [x[0] for x in cursor.description]
     data=cursor.fetchall()
@@ -82,7 +74,9 @@ def viewall():
 def insert():
     data = request.get_json()
     # print(data)
-    email = data.get('email')
+    token=request.headers.get("token")
+    decode_jwt=jwt.decode(token, secret, algorithms=['HS256'],verify=True)
+    email = decode_jwt['email']
     name=data.get('name')
     ingredients=data.get('ingredients')
     instruction=data.get('instruction')
@@ -98,50 +92,48 @@ def insert():
     # if data:
     # else:
         # return jsonify({'error':'nothing'}),201
-@app.route('/deleteone', methods=['POST'])
-@cross_origin(origins=[u"*"])
-def delete():
-    data=request.get_json()
-    print(data)
-    email=data.get('email')
-    id=data.get('id')
-    role=data.get('role')
-    print(role)
-    # decoded_jwt = jwt.decode(token, secret, algorithms=['HS256'])
-    # decoded_jwt = jwt.decode(token, secret, algorithms=['HS256'],verify=True)  
-    # print(decoded_jwt)
-    # print(decoded_jwt)
-    if role=='admin':
-        sql='delete from recipe where id=%s'
-        val=(id,)
-        cursor.execute(sql,val)
-        mysql.commit()
-        return jsonify({"message":'success'}),200
-    else:
-        if check(id,email):
-           sql = "delete from recipe where id=%s and user_id=%s"
-           val = (id,email)
-           cursor.execute(sql,val)
-           mysql.commit()
-           return jsonify({'message':'success'}),200
-        else:
-           return jsonify({'message':'not valid'}),201
 
-@app.route('/check',methods=['POST'])
+@app.route('/get_data',methods=['POST'])
 @cross_origin(origins=[u"*"])
-def check():
-    data=request.get_json()
-    email=data.get('email')
-    id=data.get('id')
-    if check(id,email):
-        return jsonify({'message':'found'}),200
+def get_all():
+    token=request.headers.get("token")
+    # print(token)
+    decode_jwt=jwt.decode(token, secret, algorithms=['HS256'],verify=True)
+    # print(decode_jwt)
+    # data=request.get_json()
+    email=decode_jwt['email']
+    # print(email)
+    # id=data.get('id')
+    role=decode_jwt['role']
+    # print(role)
+    # role="admin"
+    if role=='admin':
+        cursor.execute("select * from recipe")
+        # data=cursor.fetchall()
+        cols = [x[0] for x in cursor.description]
+        data=cursor.fetchall()
+        # print(data)
+        res = [dict(zip(cols,row)) for row in data]
+        # print(res)
+        if data:
+           return jsonify(res),200
+        else:
+            return jsonify({'error':'not found'}),201
     else:
-        return jsonify({'message':'not fount'}),201
+        cursor.execute("select * from recipe where user_id=%s",(email,))
+        # data=cursor.fetchall()
+        cols = [x[0] for x in cursor.description]
+        data=cursor.fetchall()
+        # print(data)
+        res = [dict(zip(cols,row)) for row in data]
+        if data:
+            return jsonify(res),200
+        else:
+            return jsonify({'error':'not found'}),201
+        # return jsonify({'message':'not fount'}),201    
 @app.route('/updateone',methods=['POST'])
 def updateone():
     data=request.get_json()
-    print(data)
-    email = "manishapanchal5591@gmail.com"
     name=data.get('recipe_name')
     ingredients=data.get('ingredients')
     instruction=data.get('instructions')
@@ -163,17 +155,23 @@ def updateone():
     except Exception as e:
         print(repr(e))
         return jsonify({'err':repr(e)}),500
-def check(id,email):
-        sql = "Select * from recipe where id=%s and user_id=%s"
-        val = (id,email)
-            # mycursor.execute(sql, val)
+@app.route('/delete_one',methods=['POST'])
+def delete_one():
+    data=request.get_json()
+    id=data.get('id')  
+    print(id)
+    sql = 'Delete from recipe where id=%s'
+    val = (id,)
+    try:
         cursor.execute(sql,val)
-        data=cursor.fetchall()
-        # print(data)
-        if data:
-            return 1
+        mysql.commit()
+        print(cursor.rowcount)
+        if cursor.rowcount>0:
+            return jsonify({"message":"success"}),200
         else:
-            return 0
-        
+            return jsonify({'msg':'error while'}),304
+    except Exception as e:
+        print(repr(e))
+        return jsonify({'err':repr(e)}),500       
 if __name__ == '__main__':
     app.run(debug=True)
